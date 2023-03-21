521196cb4fd1fa3f77033bea03c21348ae90cada
Taylor Boyd <taylor.boyd@oit.edu>
2023-03-20 19:33:02 -0700

    Prevented a metaclass conflict with the ClockWindow class.

--- ClockWindow.py
+++ ClockWindow.py
@@ -1,6 +1,6 @@
-import Clock
+from Clock import Clock
 from PyQt5.QtWidgets import QWidget
 
-class ClockWindow(Clock, QWidget):
+class ClockWindow(QWidget, Clock):
     def __init__(self):
         super.__init__()
