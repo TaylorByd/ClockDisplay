7b1888ccf8378461fe5756e275c4d4c6600f766f
Taylor Boyd <taylor.boyd@oit.edu>
2023-03-20 17:54:05 -0700

    Added self back as a input parameter since the functions need it to work.

--- Clock.py
+++ Clock.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 class Clock:
-    def get_time():
+    def get_time(self):
         time = datetime.now()
         current_time = time.strftime("%I:%M:%S %p")
             # formats the time to be HH:MM:SS am/pm
@@ -9,7 +9,7 @@ class Clock:
             current_time = current_time[1:]
             # if the hour of time has a 0 at the start of the string, skip that character.
         return current_time
-    def get_date():
+    def get_date(self):
         date = datetime.now()
         current_date = date.strftime("%m/%d/%Y")
             # formats the date to be MM/DD/YYYY
@@ -17,7 +17,7 @@ class Clock:
             current_date = current_date[1:]
             # if the start of the date has a 0 at the start of the string, skip the character.
         return current_date
-    def get_day():
+    def get_day(self):
         day = datetime.today()
         current_day = day.strftime("%A")
             # formats to string to show the day of the week
