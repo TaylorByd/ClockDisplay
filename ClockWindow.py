4cd0ed35d86ad8713ed015a570ffed908bc7e783
Taylor Boyd <taylor.boyd@oit.edu>
2023-03-21 20:11:38 -0700

    Created a timer to set update the text every 100 milliseconds. I also added a box layout that will center all the labels on the window.

--- ClockWindow.py
+++ ClockWindow.py
@@ -1,13 +1,26 @@
 from Clock import Clock
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QTimer, Qt
+import sys
 
 class ClockWindow(QWidget, Clock):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.time_label = QLabel()
         self.date_label = QLabel()
         self.day_label = QLabel()
+
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+        vbox.addWidget(self.time_label, alignment=Qt.AlignCenter)
+        vbox.addWidget(self.day_label, alignment=Qt.AlignCenter)
+        vbox.addWidget(self.date_label, alignment=Qt.AlignCenter)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.set_text)
+        timer.start(100)
+
     def setup_font(self, fontsize, font_type):
         font = QFont(f'{font_type}', fontsize)
         self.time_label.setFont(font)
@@ -19,6 +32,15 @@ class ClockWindow(QWidget, Clock):
         self.time_label.setStyleSheet(f"color: {font_color};")
         self.date_label.setStyleSheet(f"color: {font_color};")
     def set_text(self):
-        self.day_label.setText(Clock.get_date())
-        self.date_label.setText(Clock.get_date())
-        self.time_label.setText(Clock.get_time())
+        self.day_label.setText(Clock.get_day(self))
+        self.date_label.setText(Clock.get_date(self))
+        self.time_label.setText(Clock.get_time(self))
+
+App = QApplication(sys.argv)
+ 
+clock_window = ClockWindow()
+clock_window.setup_font(100, 'Comic Sans MS')
+clock_window.setup_color('white', 'black')
+clock_window.show()
+
+App.exit(App.exec_())
