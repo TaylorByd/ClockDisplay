51fc4c62fe47d93f5c229f08866affbd405a6cb9
Taylor Boyd <taylor.boyd@oit.edu>
2023-03-22 18:19:35 -0700

    Transfered over ClockWindow execution code.

--- ClockDisplay.py
+++ ClockDisplay.py
@@ -1,6 +1,14 @@
-from Clock import Clock
-import time
-clock = Clock()
-while True:
-    time.sleep(0.1)
-    print(clock.get_time())
+from ClockWindow import ClockWindow
+from PyQt5.QtWidgets import QApplication
+import sys
+
+App = QApplication(sys.argv)
+ 
+clock_window = ClockWindow()
+clock_window.setup_time_font(150, 'Arial')
+clock_window.setup_day_font(75, 'Arial')
+clock_window.setup_date_font(75, 'Arial')
+clock_window.setup_color('black', 'white')
+clock_window.show()
+
+App.exit(App.exec_())
