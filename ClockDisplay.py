106acd98f21e512168be90c5d795713843c413f8
Taylor Boyd <taylor.boyd@oit.edu>
2023-04-01 18:48:33 -0700

    created a window with the time as text and implemented a setup_time_font method.

--- ClockDisplay.py
+++ ClockDisplay.py
@@ -1,3 +1,4 @@
 from ClockWindow import ClockWindow
 
 clock_window = ClockWindow()
+clock_window.window.mainloop()
--- ClockWindow.py
+++ ClockWindow.py
@@ -3,16 +3,16 @@ from tkinter import Label, Tk
 
 class ClockWindow(Clock):
     def __init__(self): 
-        window = Tk()
-        self.time_label = Label(window, text= "")
-        self.date_label = Label(window, text= "")
-        self.date_label = Label(window, text= "")
-        self.time_label.pack(padx=10, pady=10)
-        window.attributes('-fullscreen', True)
-        window.mainloop()
+        self.window = Tk()
+        self.font_time = ('Arial', 150)
+        self.time_label = Label(text= "", font= self.font_time)
+        self.time_label.pack(padx=50, pady=50)
+        self.update_clock()
+        self.window.attributes('-fullscreen', True)
 
-    def setup_time_font(self, fontsize, font_type):
-        return
+    def setup_time_font(self, font_type, font_size):
+        self.font_time = (font_type, font_size)
+        self.time_label.configure(font= self.font_time)
     
     def setup_day_font(self, fontsize, font_type):
         return
@@ -32,5 +32,6 @@ class ClockWindow(Clock):
     def create_background_img(self):
         return
 
-    def set_text(self):
-        return
+    def update_clock(self):
+        self.time_label.configure(text=Clock.get_time(self))
+        self.window.after(100, self.update_clock)
