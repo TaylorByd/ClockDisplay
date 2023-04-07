7496656f3e25d80d1aa29118df8a70ca5de0a96f
Taylor Boyd <taylor.boyd@oit.edu>
2023-04-07 00:25:54 -0700

    defined height, and width of window screen, created text on canvas, and fully implemented the update_clock method.

--- ClockDisplay.py
+++ ClockDisplay.py
@@ -1,5 +1,4 @@
 from ClockWindow import ClockWindow
 
 clock_window = ClockWindow()
-clock_window.setup_color('white', 'black')
 clock_window.window.mainloop()
--- ClockWindow.py
+++ ClockWindow.py
@@ -3,9 +3,18 @@ from tkinter import Tk, Canvas
 
 class ClockWindow(Clock):
     def __init__(self): 
+        # defining the window, and the screen width and height.
         self.window = Tk()
+        self.win_width = self.window.winfo_screenwidth()
+        self.win_height = self.window.winfo_screenheight()
+        # defining the canvas with its default background color
         self.my_canvas = Canvas(self.window, bg= "black", highlightthickness=0)
+        self.my_canvas.pack(fill="both", expand=True)
+        # creating the text on the canvas
         self.font_time = ('Arial', 150)
+        self.time_label = self.my_canvas.create_text(self.win_width / 2, self.win_height / 2, 
+                                                     text= "", fill= "white", font= self.font_time)
+        # updating the clock text in intervals of 100 ms
         self.update_clock()
         self.window.attributes('-fullscreen', True)
 
@@ -28,4 +37,5 @@ class ClockWindow(Clock):
         return
 
     def update_clock(self):
+        self.my_canvas.itemconfig(self.time_label, text=Clock.get_time(self))
         self.window.after(100, self.update_clock)
