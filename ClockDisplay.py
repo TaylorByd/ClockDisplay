4715c42d99898773a9a7f5255d1515564edbdd50
Taylor Boyd <taylor.boyd@oit.edu>
2023-04-07 00:59:48 -0700

    Implemented setup background image method.

--- ClockDisplay.py
+++ ClockDisplay.py
@@ -1,6 +1,6 @@
 from ClockWindow import ClockWindow
 
 clock_window = ClockWindow()
-clock_window.setup_time_font('times new roman', 175)
-clock_window.setup_color("green", "blue")
+clock_window.setup_time_font('times new roman', 150)
+clock_window.setup_background_img("test.png")
 clock_window.window.mainloop()
--- ClockWindow.py
+++ ClockWindow.py
@@ -1,15 +1,21 @@
 from Clock import Clock
 from tkinter import Tk, Canvas
+from PIL import Image
+from PIL import ImageTk
 
 class ClockWindow(Clock):
     def __init__(self): 
+        self.background_setup = False
         # defining the window, and the screen width and height.
         self.window = Tk()
         self.win_width = self.window.winfo_screenwidth()
         self.win_height = self.window.winfo_screenheight()
         # defining the canvas with its default background color
         self.my_canvas = Canvas(self.window, bg= "black", highlightthickness=0)
+        self.bg_img = 0
         self.my_canvas.pack(fill="both", expand=True)
+        # used for deleting a background image it is already set
+        self.image_bg_id = self.my_canvas.create_image(0, 0)
         # creating the text on the canvas
         self.font_time = ('Arial', 150)
         self.time_label = self.my_canvas.create_text(self.win_width / 2, self.win_height / 2, 
@@ -29,14 +35,20 @@ class ClockWindow(Clock):
         return
    
     def setup_color(self, font_color, background_color):
+        if (self.background_setup == True):
+            self.my_canvas.delete(self.image_bg_id)
         self.my_canvas.itemconfig(self.time_label, fill= font_color)
         self.my_canvas.configure(bg= background_color)
+        self.background_setup = False
 
     def setup_background_img(self, url):
-        return
-
-    def create_background_img(self):
-        return
+        img_read = Image.open(url)
+        img_resized = img_read.resize((self.win_width, self.win_height), Image.ANTIALIAS)
+        self.bg_img = ImageTk.PhotoImage(img_resized)
+        self.image_bg_id = self.my_canvas.create_image(0, 0, image= self.bg_img, anchor= "nw")
+        self.time_label = self.my_canvas.create_text(self.win_width / 2, self.win_height / 2, 
+                                                    text= "", fill= "white", font= self.font_time)
+        self.background_setup = True
 
     def update_clock(self):
         self.my_canvas.itemconfig(self.time_label, text=Clock.get_time(self))
