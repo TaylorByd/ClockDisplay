87d908764c0af1e5d8e849e5885481574aa2c217
Taylor Boyd <taylor.boyd@oit.edu>
2023-04-07 00:16:55 -0700

    Replaced label with canvas in order to implement background images.

--- ClockWindow.py
+++ ClockWindow.py
@@ -1,18 +1,16 @@
 from Clock import Clock
-from tkinter import Label, Tk
+from tkinter import Tk, Canvas
 
 class ClockWindow(Clock):
     def __init__(self): 
         self.window = Tk()
+        self.my_canvas = Canvas(self.window, bg= "black", highlightthickness=0)
         self.font_time = ('Arial', 150)
-        self.time_label = Label(text= "", font= self.font_time)
-        self.time_label.pack(padx=50, pady=50)
         self.update_clock()
         self.window.attributes('-fullscreen', True)
 
     def setup_time_font(self, font_type, font_size):
-        self.font_time = (font_type, font_size)
-        self.time_label.configure(font= self.font_time)
+        return
     
     def setup_day_font(self, fontsize, font_type):
         return
@@ -21,8 +19,7 @@ class ClockWindow(Clock):
         return
    
     def setup_color(self, font_color, background_color):
-        self.time_label.configure(bg= background_color, fg = font_color)
-        self.window.configure(bg= background_color)
+        return
 
     def setup_background_img(self, url):
         return
@@ -31,5 +28,4 @@ class ClockWindow(Clock):
         return
 
     def update_clock(self):
-        self.time_label.configure(text=Clock.get_time(self))
         self.window.after(100, self.update_clock)
